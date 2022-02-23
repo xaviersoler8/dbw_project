de871708f1d72d9962b260ff2dab2f4f64d71160
juliavilmor <93728952+juliavilmor@users.noreply.github.com>
2022-02-23 13:34:56 +0100

    exp_out

--- mylab/experiments_out.php
+++ mylab/experiments_out.php
@@ -52,65 +52,67 @@
   </div>
 </nav>
 
-
-<?php
-$connection = mysqli_connect("localhost", "mylab", "dbmylab", "login_register_db" );
-$type=$_POST["typeref"];
-$amounnt=$_POST["tentacles"];
-$user=$_POST["userref"];
-
-
-$insertar="INSERT INTO prueba(type,amount,uwu) VALUES ('$type','$amounnt','$user')";
-$resultado=mysqli_query($connection,$insertar);
-
-
-$valor_restar="SELECT * FROM protocols WHERE type='$type' AND user='$user'";
-$x=mysqli_query($connection,$valor_restar);
-
-$row = mysqli_fetch_assoc($x);
-echo "<br>";
-echo $row[steps];
-echo "<br><br>";
-$count=0;
-
-foreach (array_keys($row) as $key){
-
-    $valor_before="SELECT amount FROM reactives WHERE name ='$key'";
-    $z=mysqli_query($connection,$valor_before);
-    $uwu=mysqli_fetch_assoc($z);
-    $count=$count+1;
-    if ($count >= 3){
-      if ($row[$key]<>0){
-      echo "The amount of ";
-      echo $key;    
-      echo " to use is: ";
-      echo $row[$key]*$amounnt."<br>";
-      }
-    }
-    $new_valor=($uwu['amount']-(($row[$key])*$amounnt));
-    
-    $actualizar="UPDATE reactives SET amount=$new_valor WHERE name='$key'";
-    $y=mysqli_query($connection,$actualizar);
-};
-
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-	<link href="experiments.css" rel="stylesheet">
-  <script data-require="jquery@3.1.1" data-semver="3.1.1" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-  <script src="mylab/js/scriptbutton.js"></script>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-</head>
 <body>
+
   <main role="main" class="container">
+
+  <!-- PHP -->
+  <?php
+    $connection = mysqli_connect("localhost", "mylab", "dbmylab", "login_register_db" );
+    $type=$_POST["typeref"];
+    $amounnt=$_POST["tentacles"];
+    $user=$_POST["userref"];
+
+
+    $insertar="INSERT INTO prueba(type,amount,uwu) VALUES ('$type','$amounnt','$user')";
+    $resultado=mysqli_query($connection,$insertar);
+
+
+    $valor_restar="SELECT * FROM protocols WHERE type='$type' AND user='$user'";
+    $x=mysqli_query($connection,$valor_restar);
+    
+    $row = mysqli_fetch_assoc($x);
+  ?>
+  <h1>
+    <br>
     <?php
     echo $row[type];
+    echo "<br>";
     ?>
+  </h1>
+  <h3> STEPS </h3>
+  <p>
+    <?php
+    echo $row[steps];
+    echo "<br><br>";
+    $count=0;
+    ?>
+  </p>
+  <h3> AMOUNTS </h3>
+  <p>
+    <?php
+    foreach (array_keys($row) as $key){
+
+        $valor_before="SELECT amount FROM reactives WHERE name ='$key'";
+        $z=mysqli_query($connection,$valor_before);
+        $uwu=mysqli_fetch_assoc($z);
+        $count=$count+1;
+        if ($count >= 3){
+          if ($row[$key]<>0){
+          echo "The amount of ";
+          echo $key;    
+          echo " to use is: ";
+          echo $row[$key]*$amounnt."<br>";
+          }
+        }
+        $new_valor=($uwu['amount']-(($row[$key])*$amounnt));
+        
+        $actualizar="UPDATE reactives SET amount=$new_valor WHERE name='$key'";
+        $y=mysqli_query($connection,$actualizar);
+        };
+    ?>
+
+  </p>  
 
     <br>
     <button class="btn btn-dark" onclick="history.back()">Go back</button>
